ccafe2aef2f9fb0f13412052f3df3e6806779d6a
Slobodan Dan <danastroman@gmail.com>
2023-02-05 21:27:30 +0100

    Trasnpiling using esbuild.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
+    const serviceRef = useRef<any>();
 
-    const onClick = () => {
-        console.log(input);
+    const onClick = async () => {
+        if (!serviceRef.current) return;
+
+        const result = await serviceRef.current.transform(input, {
+            loader: "jsx",
+            target: "es2015",
+        });
+        setCode(result.code);
     };
+
+    const startService = async () => {
+        serviceRef.current = await esbuild.startService({
+            worker: true,
+            wasmURL: "/esbuild.wasm",
+        });
+    };
+
+    useEffect(() => {
+        startService();
+    }, []);
 
     return (
         <div>
