46111e0c20e0d44a6a9954b650ddace682d31e24
Slobodan Dan <danastroman@gmail.com>
2023-02-07 09:46:38 +0100

    Caching packages.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,6 +1,11 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
- 
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+    name: "filecache"
+});
+
 export const unpkgPathPlugin = () => {
     return {
         name: 'unpkg-path-plugin',
@@ -38,13 +43,21 @@ export const unpkgPathPlugin = () => {
                     };
                 }
 
-                const { data, request } = await axios.get(args.path);
+                const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-                return {
+                if (cachedResult)
+                    return cachedResult;
+
+                const { data, request } = await axios.get(args.path);
+                const result: esbuild.OnLoadResult = {
                     loader: 'jsx',
                     contents: data,
                     resolveDir: new URL('./', request.responseURL).pathname
                 }
+
+                await fileCache.setItem(args.path, result);
+
+                return result;
             });
         },
     };
