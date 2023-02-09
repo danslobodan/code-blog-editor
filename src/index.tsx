c15eb204013ec78d9eee5290c93933db9b30ff59
Slobodan Dan <danastroman@gmail.com>
2023-02-09 11:25:34 +0100

    Extracted bundler component.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 
-import * as esbuild from "esbuild-wasm";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import bundle from "./bundler";
 
 import Preview from "./components/preview";
 
@@ -13,35 +11,10 @@ const App = () => {
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
 
-    const serviceRef = useRef<any>();
-
     const onClick = async () => {
-        if (!serviceRef.current) return;
-
-        const result = await serviceRef.current.build({
-            entryPoints: ["index.js"],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                "process.env.NODE_ENV": "'production'",
-                global: "window",
-            },
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundle(input);
+        setCode(output);
     };
-
-    const startService = async () => {
-        serviceRef.current = await esbuild.startService({
-            worker: true,
-            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-        });
-    };
-
-    useEffect(() => {
-        startService();
-    }, []);
 
     return (
         <div>
